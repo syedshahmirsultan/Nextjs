95ee2f8cfe69da6fdd2640c07b73a9fe8cea2ff4
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-21 02:29:32 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function App(){
 </Box><Heading className='heading2'>Program of Studies</Heading>
 <Text as='p' className='program'>This curriculum is intended for beginners who want to learn software development from the ground up. The first three quarters are shared by all specialties and are dedicated to studying Object-Oriented Programming and cutting-edge Full-Stack Web 2.0 development. It is going to be a fifteen-month-long hybrid program that includes both onsite and online classes and is divided into five quarters of 13 weeks each. The emphasis will be on hands-on learning by educating students to produce projects. To accommodate everyone, courses will be held primarily on weekends or after 6:00 p.m. (Pakistan Time) on weekdays. It employs a hybrid teaching format, with core onsite classes complemented by online Zoom laboratories and recorded videos.
 </Text>
-<Image className='laptop' src="/images/laptop.jpg" alt='picture of laptop' height='500' width='650'/>
+<Image className='laptop' src="/images/laptop.jpg" alt='picture of laptop' height='300' width='500'/>
 <Heading className='heading3'>Relevant Information Links</Heading>
 <Text><Link className='links' href="https://www.piaic.org/">Addmission Website</Link></Text>
 <Text><Link className='links1' href="https://web.facebook.com/groups/panaverse?_rdc=1&_rdr">Facebook Group</Link></Text>
